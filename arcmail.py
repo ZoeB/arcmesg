7dd9a59523a194475ae7dd41233ce1a3553c9140
Zoë Blade <zoe@bytenoise.co.uk>
2013-04-29 10:15:33 +0100

    Remove troublesome bits
    For some reason, Arch Linux on my Raspberry Pi doesn't seem to like
    the chmodding bits.

--- arcmail.py
+++ arcmail.py
@@ -10,7 +10,7 @@ if not os.path.exists(os.path.expanduser(configFile)):
 	exit()
 
 if not os.path.exists(os.path.expanduser(outputDir)):
-	os.mkdir(os.path.expanduser(outputDir), 0755)
+	os.mkdir(os.path.expanduser(outputDir))
 
 config = open(os.path.expanduser(configFile))
 
@@ -41,7 +41,7 @@ for line in csv.reader(config, delimiter='\t'):
 				hashFile = hashedMessageID[2:]
 
 				if not os.path.exists(os.path.expanduser(outputDir+'/'+hashDir)):
-					os.mkdir(os.path.expanduser(outputDir+'/'+hashDir), 0755)
+					os.mkdir(os.path.expanduser(outputDir+'/'+hashDir))
 
 				emailFile = open(os.path.expanduser(outputDir+'/'+hashDir+'/'+hashFile), 'w')
 
