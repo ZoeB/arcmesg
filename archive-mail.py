7dff86b636ae4454bf41369078b3ac00a4bf5b31
Zoë Blade <zoe@bytenoise.co.uk>
2013-04-29 08:59:41 +0100

    Start work on reading configuration file

--- archive-mail.py
+++ archive-mail.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python
+
+import os
+
+configFile = '~/.archive-mail'
+
+if not os.path.exists(os.path.expanduser(configFile)):
+	print('Please create configuration file', configFile);
+	exit()
+
+config = open(os.path.expanduser(configFile))
+
+for line in config:
+	if line[0] == '#':
+		continue
+
+	print(line)
