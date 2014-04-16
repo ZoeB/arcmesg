f943154b6f95c9283fcfd0110f11c53c22c0180f
Zoë Blade <zoe@bytenoise.co.uk>
2014-04-16 09:50:36 +0100

    Improve searching script
    Ahem, this is a *lot* simpler...

--- search.sh
+++ search.sh
@@ -3,17 +3,4 @@
 echo "Search query?"
 read string
 
-for outerDir in ??
-do
-	for innerDir in $outerDir/??
-	do
-		for file in $innerDir/*
-		do
-			result=$(grep "$string" $file)
-
-			if [ ${#result} -ne 0 ]; then
-				echo $file $result
-			fi
-		done
-	done
-done
+find * -type f | xargs grep "$string"
