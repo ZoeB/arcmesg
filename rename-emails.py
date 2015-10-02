bdbd50499671f28f2d72ada5cb6d57672b0f7a56
Zoë Blade <zoe@bytenoise.co.uk>
2015-10-02 10:52:07 +0100

    Write quick script to rename e-mails to their IDs

---
+++ rename-emails.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Rename e-mail files to their message ID, by Zoe Blade
+# For Python 3
+# TODO: If it has more than one message ID in it, don't rename it.
+
+import re, os
+
+for filename in os.listdir('.'):
+	if not os.path.isfile(filename):
+		continue
+
+	file = open(filename, 'r', 1, 'iso-8859-1')
+
+	for line in file:
+		result = re.search('Message-ID: <([^>]+)>', line, flags=re.IGNORECASE)
+
+		if (result and result.group(1) and '@' in result.group(1)):
+			os.rename(filename, result.group(1))
+			break
