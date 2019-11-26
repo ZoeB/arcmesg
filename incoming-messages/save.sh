20fbc9924870bf0076c35b4bde2adf76da7969e2
Zoë Blade <zoe@bytenoise.co.uk>
2019-11-26 09:51:19 +0000

    Fix
    Tildas aren't allowed here.

--- incoming-messages/save.sh
+++ incoming-messages/save.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 number=1
-filename="~/incoming-messages/${number}.txt"
+filename="/home/username/incoming-messages/${number}.txt"
 
 while [ -f $filename ]; do
 	number=$(( $number + 1 ))
-	filename="~/incoming-messages/${number}.txt"
+	filename="/home/username/incoming-messages/${number}.txt"
 done
 
 tee $filename
